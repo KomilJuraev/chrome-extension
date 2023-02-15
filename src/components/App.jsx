dfbd91c15dbc0a62fb29c3188cc036869d5370a9
KomilJuraev <K.juraev1994@gmail.com>
2023-02-14 21:50:55 -0500

    completed implementing local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
     const [name, setName] = useState();
@@ -6,6 +6,27 @@ function App() {
     const [nameList, setNameList] = useState([]);
     const [urlList, setUrlList] = useState([]);
     const [checkboxId, setCheckBoxId] = useState([]);
+    const [checked, setChecked] = useState();
+    const [errorMsg, setErrorMsg] = useState();
+
+    useEffect(() => {
+        const myNames = JSON.parse(localStorage.getItem('nameList'));
+        const myUrls = JSON.parse(localStorage.getItem('urlList'));
+
+        if (myNames !== null && myNames !== undefined && myNames.length > 0) {
+            setNameList(myNames);
+        }
+
+        if (myUrls !== null && myUrls !== undefined && myUrls.length > 0) {
+            setUrlList(myUrls);
+        }
+
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('nameList', JSON.stringify(nameList));
+        localStorage.setItem('urlList', JSON.stringify(urlList));
+    }, [nameList, urlList]);
 
     function handleNameChange(event) {
         setName(event.target.value);
@@ -16,18 +37,34 @@ function App() {
     }
 
     function saveInput() {
-        setNameList([...nameList, name]);
-        setName();
-        setUrlList([...urlList, url]);
-        setUrl();
+        if (name === undefined || name === "" || name === " ") {
+            if (url === undefined || url === "" || url === " ") {
+                setErrorMsg("Please enter a value into Name and Url input boxes");
+            } else {
+                setErrorMsg("Please enter a value into Name input box");
+            }
+        } else if (url === undefined || url === "" || url === " ") {
+            setErrorMsg("Please enter a value into Url input box");
+        } else {
+            setErrorMsg();
+            setNameList([...nameList, name]);
+            setName("");
+            setUrlList([...urlList, url]);
+            setUrl("");
+        }
     }
 
     function saveTab() {
-        setNameList([...nameList, name]);
-        setName();
-        let currentUrl = window.location.href;
-        setUrlList([...urlList, currentUrl]);
-        console.log(urlList);
+        if (name === undefined || name === "" || name === " ") {
+            setErrorMsg("Please enter a value into Name input box");
+        } else {
+            setErrorMsg();
+            setNameList([...nameList, name]);
+            setName("");
+            let currentUrl = window.location.href;
+            setUrlList([...urlList, currentUrl]);
+            setUrl("");
+        }
     }
 
     function handleCheck(event) {
@@ -35,29 +72,41 @@ function App() {
         let checkId = event.target.id;
         let id = parseInt(checkId.substring(16));
         setCheckBoxId([...checkboxId, id]);
-
+        // setChecked(true)
     }
 
     function deleteSingleUrl() {
-        const newNameLst = nameList.filter((eachName, index) => { return !checkboxId.includes(index) });
-        const newUrlLst = urlList.filter((eachUrl, index) => { return !checkboxId.includes(index) })
-        setNameList(newNameLst);
-        setUrlList(newUrlLst)
-        setCheckBoxId([]);
+        if (checkboxId.length === 0) {
+            setErrorMsg("Please Select a URL to Delete");
+        } else {
+            const newNameLst = nameList.filter((eachName, index) => { return !checkboxId.includes(index) });
+            const newUrlLst = urlList.filter((eachUrl, index) => { return !checkboxId.includes(index) })
+            setNameList(newNameLst);
+            setUrlList(newUrlLst)
+            setCheckBoxId([]);
+            setChecked(false);
+        }
+    }
+
+    function deleteAll() {
+        setErrorMsg("");
+        setNameList([]);
+        setUrlList([]);
+        localStorage.clear();
     }
 
     return (
         <div id="parent-div">
             <div id="input-sec">
-                <input type="text" id="key-el" placeholder="Name" value={name} onChange={handleNameChange} />
-                <input type="text" id="input-el" placeholder="URL" value={url} onChange={handleUrlChange} />
+                <input type="text" id="key-el" placeholder="Name" value={name} onChange={handleNameChange} onFocus={() => { setName("") }} />
+                <input type="text" id="input-el" placeholder="URL" value={url} onChange={handleUrlChange} onFocus={() => { setUrl("") }} />
             </div>
-            <p id="error-el"></p>
+            <p id="error-el">{errorMsg}</p>
             <div id="btn-sec">
                 <button id="input-btn" onClick={saveInput}>SAVE INPUT</button>
                 <button id="tab-btn" onClick={saveTab}>SAVE TAB</button>
                 <button id="delete-btn" onClick={deleteSingleUrl}>DELETE</button>
-                <button id="delete-all-btn">DELETE ALL</button>
+                <button id="delete-all-btn" onDoubleClick={deleteAll}>DELETE ALL</button>
             </div>
             <ul id="ul-el">
                 {nameList.map((eachName, index) => (
@@ -65,6 +114,7 @@ function App() {
                         <input
                             id={'checkbox-number-' + index}
                             type='checkbox'
+                            checked={checked}
                             onChange={handleCheck}
                         />
                         <span id="urlName">
